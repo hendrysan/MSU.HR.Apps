4280cfc0cd6d6da79f200505e79d8746e5cc3f90
Hendry Priyatno <hendry.priyatno@gmail.com>
2023-11-20 20:02:24 +0700

    temp add minio

---
+++ Solution/Commons/Utilities/MinioUtility.cs
@@ -0,0 +1,63 @@
+﻿using Minio;
+using Minio.DataModel.Args;
+using Minio.Exceptions;
+
+namespace Commons.Utilities
+{
+    public class MinioUtility
+    {
+
+        private static async Task<IMinioClient> BuildMinioClinet()
+        {
+            string endpoint = "192.168.1.8:9100";
+            var accessKey = "s7YywmlgTmvQENE959gu";
+            var secretKey = "FFJgGUUrjvbAj97feOcVKnJ8SmBcySQo1c7iCLKK";
+
+            IMinioClient minio = new MinioClient()
+                                    .WithEndpoint(endpoint)
+                                    .WithCredentials(accessKey, secretKey)
+                                    .Build();
+
+            return minio;
+        }
+
+        public static async Task<bool> SendAsync()
+        {
+            var bucketName = "test";
+            var objectName = "Setting VPN v2.jpeg";
+            var filePath = "C:\\Users\\Hendry Priyatno\\Documents\\Project Ongoing\\BitHealth\\Setting VPN v2.jpeg";
+            var contentType = "application/JPEG";
+
+            try
+            {
+                var minio = await BuildMinioClinet();
+                // Make a bucket on the server, if not already present.
+                var beArgs = new BucketExistsArgs()
+                    .WithBucket(bucketName);
+                bool found = await minio.BucketExistsAsync(beArgs).ConfigureAwait(false);
+                if (!found)
+                {
+                    var mbArgs = new MakeBucketArgs()
+                        .WithBucket(bucketName);
+                    await minio.MakeBucketAsync(mbArgs).ConfigureAwait(false);
+                }
+                // Upload a file to bucket.
+                var putObjectArgs = new PutObjectArgs()
+                    .WithBucket(bucketName)
+                    .WithObject(objectName)
+                    .WithFileName(filePath)
+                    .WithContentType(contentType);
+                await minio.PutObjectAsync(putObjectArgs).ConfigureAwait(false);
+                Console.WriteLine("Successfully uploaded " + objectName);
+
+                return true;
+            }
+            catch (MinioException ex)
+            {
+                Console.WriteLine("File Upload Error: {0}", ex.Message);
+                await Loggers.DiscordLogger.SendAsync("MinioUtility", ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ Solution/UnitTest/CommonTest/MinioUtilityUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Commons.Utilities;
+
+namespace UnitTest.CommonTest
+{
+    public class MinioUtilityUnitTest
+    {
+
+        [Fact]
+        public async Task Send()
+        {
+            //List<string> recipients = new List<string>();
+            //recipients.Add("hendry.priyatno@gmail.com");
+            //string subject = "test kirim email " + DateTime.Now.ToString();
+            //string body = "body content email";
+
+            //var status = await MailUtility.SendAsync(recipients, subject, body);
+            //Assert.True(status);
+
+            var task = await MinioUtility.SendAsync();
+        }
+    }
+}
